77d95ba4754811f37609f1b84d2cbfde6f68bf7b
Brett Mastbergen <bmastbergen@ciq.com>
2025-08-01 16:08:16 -0400

    Break the kabi

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -691,6 +691,7 @@ struct perf_event {
 	int				event_caps;
 	/* The cumulative AND of all event_caps for events in this group. */
 	int				group_caps;
+	unsigned int			group_generation;
 
 	struct perf_event		*group_leader;
 	struct pmu			*pmu;
@@ -805,7 +806,6 @@ struct perf_event {
 #endif
 	struct list_head		sb_list;
 
-	RH_KABI_EXTEND(unsigned int	group_generation)
 #endif /* CONFIG_PERF_EVENTS */
 };
 
